15b9c563788764b5fdf266d5706e6433f546473d
lucspt <149544385+lucspt@users.noreply.github.com>
2024-05-11 17:04:09 -0700

    update gh pages workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/portfolio/",
-  plugins: [react()],
-  server: {
-    port: 3000
+export default defineConfig(({ command }) => {
+  
+  return {
+    base: command === "serve" ? "/portfolio/" : "/",
+    plugins: [react()],
+    server: {
+      port: 3000
+    }
   }
+  
 })
