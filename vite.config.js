08fb236ef25af919b4fa5f0d581da8416912f583
lucspt <149544385+lucspt@users.noreply.github.com>
2024-05-11 17:06:17 -0700

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   
   return {
-    base: command === "serve" ? "/portfolio/" : "/",
+    base: command === "build" ? "/portfolio/" : "/",
     plugins: [react()],
     server: {
       port: 3000
